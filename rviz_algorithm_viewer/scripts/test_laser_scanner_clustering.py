95af2238e22225a30642202482f05beba5aac773
David <david.bitonneau@gmail.com>
2013-09-12 19:58:15 -0400

    Adding test working with the laser scanner
    Subscribes to /scan topic

--- rviz_algorithm_viewer/scripts/test_laser_scanner_clustering.py
+++ rviz_algorithm_viewer/scripts/test_laser_scanner_clustering.py
@@ -3,17 +3,17 @@
 import roslib; roslib.load_manifest( 'rviz_algorithm_viewer' )
 import rospy
 from rviz_algorithm_viewer.msg import Cluster2, ClusterField
+from sensor_msgs.msg import LaserScan
 import geometry_msgs.msg
-import tf
+import tf; br = tf.TransformBroadcaster()
+pub = rospy.Publisher( 'test_cluster', Cluster2 )
 
 import cv
 import cv2
 import numpy as np
+import math
 
-points_number = 100
-cluster_number = 20
-range_size = 20
-fps = 10
+cluster_number = 10
 
 def get_points_label( pts ):
   retval, bestLabels, centers = cv2.kmeans(
@@ -21,7 +21,8 @@ def get_points_label( pts ):
       K=cluster_number,
       criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_MAX_ITER, 1, 10),
       attempts=1,
-      flags=cv2.KMEANS_RANDOM_CENTERS)
+      flags=cv2.KMEANS_PP_CENTERS)
+      #flags=cv2.KMEANS_RANDOM_CENTERS)
 
   return bestLabels
 
@@ -49,39 +50,42 @@ def gen_cluster_msg( pts, labels ):
 
   return clust_msg
 
-def send_clusters():
-  pub = rospy.Publisher( 'test_cluster', Cluster2 )
+def send_clusters( points ):
+  pub.publish( gen_cluster_msg( points, get_points_label( points ) ))
+
+  br.sendTransform((0, 0, 0),
+      tf.transformations.quaternion_from_euler(0, 0, 0),
+      rospy.Time.now(),
+      "base_link",
+      "map")
+
+def get_laser_scan_pts( msg ):
+  i = 0;
+
+  points = []
+  theta = msg.angle_min
+  #for theta in np.arange(, , ):
+  while theta < ( msg.angle_max - msg.angle_increment/2 ):
+    if i < len( msg.ranges ):
+      r = msg.ranges[i];
+      x = r * math.cos(theta);
+      y = r * math.sin(theta);
+
+      points.append([ x, y, 0 ])
+
+      theta += msg.angle_increment
+      i += 1
+
+  send_clusters( np.asarray( points ))
+
+def listen_and_send_clusters():
   rospy.init_node( 'test_cluster' )
-
-  rospy.loginfo( 'Cluster test started. Sending cluster points...' )
-
-  br = tf.TransformBroadcaster()
-
-  while not rospy.is_shutdown():
-    #points = np.random.randn( points_number, 3 )
-    #points = points / 2.5 * range_size
-
-    points = []
-    for i in range( points_number ):
-      points.append( [
-          np.random.randint( -1 * range_size, range_size ),
-          np.random.randint( -1 * range_size, range_size ),
-          np.random.randint( -1 * range_size, range_size ) ] )
-
-    points = np.asarray( points )
-    pub.publish( gen_cluster_msg( points, get_points_label( points ) ))
-
-    br.sendTransform((0, 0, 0),
-        tf.transformations.quaternion_from_euler(0, 0, 0),
-        rospy.Time.now(),
-        "base_link",
-        "map")
-
-    rospy.sleep( 1. / fps )
-
+  rospy.Subscriber( "scan", LaserScan, get_laser_scan_pts )
+  rospy.loginfo( 'Cluster test started. Reading points from scan /topic and sending cluster points...' )
+  rospy.spin()
 
 if __name__ == '__main__':
   try:
-    send_clusters()
+    listen_and_send_clusters()
   except rospy.ROSInterruptException:
     pass
